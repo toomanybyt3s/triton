d74b61be9c48f3639614020320538a2b4b5e42a8
toomanybytes <therealtoomanybytes@gmail.com>
2020-12-01 10:10:22 +0000

    Changed framwork to echo

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/toomanybyt3s/triton
+
+go 1.15
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+)
--- server.go
+++ server.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os/exec"
 	"strings"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	app := fiber.New()
+	e := echo.New()
+	e.GET("/", root)
+	e.GET("/mc/:ip", mc)
+	e.Logger.Fatal(e.Start(":9090"))
+}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+func root(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World 👋!")
+}
 
-	app.Get("/mc/*", func(c *fiber.Ctx) error {
-		ip := c.Params("*")
-		out, _ := exec.Command("ping", ip, "-c 4", "-i 1", "-w 10").Output()
-		if strings.Contains(string(out), "Destination Host Unreachable") {
-			msg := fmt.Sprintf("Its down :(")
-			return c.SendString(msg)
-		}
-		msg := fmt.Sprintf("IT'S ALIVEEE")
-		return c.SendString(msg)
-	})
-
-	app.Listen(":9090")
+func mc(c echo.Context) error {
+	ip := c.Param("ip")
+	out, _ := exec.Command("ping", ip, "-c 4", "-i 1", "-w 5").Output()
+	if strings.Contains(string(out), "Destination Host Unreachable") {
+		msg := fmt.Sprintf("Its down :(")
+		return c.String(http.StatusOK, msg)
+	}
+	msg := fmt.Sprintf("IT'S ALIVEEE")
+	return c.String(http.StatusOK, msg)
 }
